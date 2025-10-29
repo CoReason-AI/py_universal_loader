3ac26ce41a2d24ac70c2039ca5a797fb10dba449
Gowtham Rao <gowtham.rao@coreason.ai>
2025-10-29 08:22:06 -0400

    chore: apply updated license header to all Python files (#19)

--- tests/test_mysql_loader.py
+++ tests/test_mysql_loader.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 CoReason, Inc.
+# Copyright (c) 2025 CoReason, Inc
 #
 # This software is proprietary and dual-licensed.
 # Licensed under the Prosperity Public License 3.0 (the "License").
@@ -6,7 +6,7 @@
 # For details, see the LICENSE file.
 # Commercial use beyond a 30-day trial requires a separate license.
 #
-# Source Code: https://github.com/CoReason-AI/py_universal_loader
+# Source Code: https://github.com/CoReason-AI/py_parquet_forget
 
 from unittest.mock import patch, MagicMock
 import pandas as pd
